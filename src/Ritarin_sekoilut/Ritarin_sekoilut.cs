2150593658dfdec69bbf0cc6137462476af5748f
rustling_leaves <rustling.tealeaves@gmail.com>
2026-02-03 22:45:07 +0200

    Print action options for player

--- src/Ritarin_sekoilut/Ritarin_sekoilut.cs
+++ src/Ritarin_sekoilut/Ritarin_sekoilut.cs
@@ -10,8 +10,17 @@
         // Makes the player choose a valid action from choices available
         static void ChooseAction()
         {
-            // Contents here
+            Console.ResetColor(); // Reset colours, just in case
+            Console.ForegroundColor = ConsoleColor.DarkYellow; // Change the colour of the text to dark yellow
+            Console.WriteLine("Now, what shall you do?");
+            Console.ForegroundColor = ConsoleColor.Yellow; // Change the colour of the text to yellow
+            Console.WriteLine("1 - Attack with your sword");
+            Console.WriteLine("2 - Defend with your shield");
+            Console.ResetColor(); // Reset colours
 
+            // Define variables for the different options
+            // string actionAttack = "1 - Attack with your sword";
+            // string actionDefend = "2 - Defend with your shield";
         }
     }
 }
