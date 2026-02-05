d6d293054008a6ab155a30cc9fd4b5f3aa1d561c
rustling_leaves <rustling.tealeaves@gmail.com>
2026-02-05 12:15:18 +0200

    Add base structure to DoorMechanism

--- src/DoorMechanism/Program.cs
+++ src/DoorMechanism/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        // Enums for door states and player actions
+        enum DoorState
+        {
+            Open,
+            Closed,
+            Locked
+        }
+        // Numerical values allow for conversion to integer and back
+        enum PlayerAction
+        {
+            Open = 0,
+            Lock = 1,
+            Unlock = 2,
+            Close = 3
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Set the starting state for the door
+            DoorState state = DoorState.Open;
+        }
+
+        static void DoorOpen(PlayerAction action)
+        {
+            // Hmm could it be done this way instead of four different methods?
         }
     }
 }
