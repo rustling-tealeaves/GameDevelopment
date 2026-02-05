be8ad2c79f9a02e962b676f657906c817ddc8a17
rustling_leaves <rustling.tealeaves@gmail.com>
2026-02-05 12:32:41 +0200

    Add more structure to DoorMechanism.cs
    Base structure for methods I currently plan to use in the code.

--- src/DoorMechanism/DoorMechanism.cs
+++ src/DoorMechanism/DoorMechanism.cs
@@ -21,12 +21,37 @@
         static void Main(string[] args)
         {
             // Set the starting state for the door
-            DoorState state = DoorState.Open;
+            DoorState doorState = DoorState.Open;
+            // Placeholder so the methods I'm working on are bright in the editor
+            DoorAction();
         }
 
-        static void DoorOpen(PlayerAction action)
+        static void DoorAction()
         {
             // Hmm could it be done this way instead of four different methods?
+            // Or perhaps... both?
+            // Placeholders so the methods I'm working on are bright in the editor
+            DoorOpen();
+            DoorLock();
+            DoorUnlock();
+            DoorClose();
+        }
+
+        static void DoorOpen()
+        {
+            Console.WriteLine("You try to open the door.");
+        }
+        static void DoorLock()
+        {
+            Console.WriteLine("You try to lock the door.");
+        }
+        static void DoorUnlock()
+        {
+            Console.WriteLine("You try to unlock the door.");
+        }
+        static void DoorClose()
+        {
+            Console.WriteLine("You try to close the door.");
         }
     }
 }
