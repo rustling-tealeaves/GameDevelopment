ab63192252b08c908004a4650cda31b7d35274e2
rustling_leaves <rustling.tealeaves@gmail.com>
2026-02-24 23:33:37 +0200

    Ritarin_sekoilut, fluff and colours
    Now includes introductory fluff and fancy colours.

--- src/Ritarin_sekoilut/Ritarin_sekoilut.cs
+++ src/Ritarin_sekoilut/Ritarin_sekoilut.cs
@@ -25,6 +25,7 @@ namespace Ritarin_sekoilut
         // Main gameplay loop, neatly inside a function
         static void SimulateGame(int hpKnight, int hpOrc, Random random)
         {
+            PrintIntroFluff();
             while (hpKnight > 0 && hpOrc > 0)
             {
                 PrintSituation(hpKnight, hpOrc); // Prints two lines, telling the situation
@@ -35,6 +36,18 @@ namespace Ritarin_sekoilut
                 hpOrc = hpOrcNew;
             }
             PrintSituation(hpKnight, hpOrc);
+        }
+
+        // Prints the chosen intro fluff; change contents if needed
+        static void PrintIntroFluff()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("You are an aspiring knight, \non a foolhardy quest to slay an orc.");
+            Console.WriteLine("Your enemy is the first to notice you.");
+            Console.WriteLine("You've no chance to escape.");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("May you prevail!");
+            Console.ResetColor();
         }
 
         // Prints the current HP situation
@@ -49,10 +62,10 @@ namespace Ritarin_sekoilut
         static string ChooseAction()
         {
             Console.ResetColor(); // Reset colours, just in case
-            Console.ForegroundColor = ConsoleColor.Yellow; // Change text colour to yellow
+            Console.ForegroundColor = ConsoleColor.Yellow; // Set text colour to yellow
             Console.WriteLine("1 - Attack with your sword");
             Console.WriteLine("2 - Defend with your shield");
-            Console.ForegroundColor = ConsoleColor.DarkYellow; // Change text colour to dark yellow
+            Console.ForegroundColor = ConsoleColor.DarkYellow; // Set text colour to dark yellow
             Console.Write("Now, what shall you do? ");
             Console.ResetColor(); // Reset colours
 
@@ -63,17 +76,23 @@ namespace Ritarin_sekoilut
                 // Make sure the input is valid
                 if (chosenAction == "1")
                 {
+                    Console.ForegroundColor = ConsoleColor.Green; // Set text colour to green
                     Console.WriteLine("You attack with your sword!");
+                    Console.ResetColor(); // Reset colours
                     return chosenAction;
                 }
                 else if (chosenAction == "2")
                 {
+                    Console.ForegroundColor = ConsoleColor.Green; // Set text colour to green
                     Console.WriteLine("You raise your shield in defense!");
+                    Console.ResetColor(); // Reset colours
                     return chosenAction;
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Cyan; // Set text colour to cyan
                     Console.WriteLine("Invalid action, try again.");
+                    Console.ResetColor(); // Reset colours
                 }
             }
 
@@ -96,7 +115,9 @@ namespace Ritarin_sekoilut
 
             // Otherwise, make the attack happen (soonTM)
             int damage = RollDamageDie(random); // An oddly complicated Random.Next
+            Console.ForegroundColor = ConsoleColor.Green; // Set text colour to green
             Console.WriteLine($"Your attack lands. The orc takes {damage} damage.");
+            Console.ResetColor(); // Reset colours
             return Math.Clamp(hpOrc - damage, 0, MaxHpOrc); // Don't go below 0
         }
 
@@ -104,6 +125,7 @@ namespace Ritarin_sekoilut
         static int CalculateNewHpKnight(int hpKnight, string chosenAction, Random random)
         {
             // The orc attacks, every time.
+            Console.ForegroundColor = ConsoleColor.Red; // Set text colour to red
             Console.WriteLine("The orc attacks you with its club!");
 
             // Calculate base damage
@@ -122,6 +144,7 @@ namespace Ritarin_sekoilut
                 Console.WriteLine($"The club hits your shield, causing only {damage} damage to you.");
             }
 
+            Console.ResetColor(); // Reset colours
             return Math.Clamp(hpKnight - damage, 0, MaxHpKnight);
         }
     }
